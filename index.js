ff950889f41c77eae66a370dca34a911ba3b1625
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-01 18:42:33 +0300

    Add Linters

--- index.js
+++ index.js
@@ -2,51 +2,50 @@ import Store from './modules/store.js';
 import Book from './modules/book.js';
 import { DateTime } from './node_modules/luxon/src/luxon.js';
 
-
 class UI {
-    static displayBooks() {
-      const books = Store.getBooks();
-      const bookList = document.getElementById('book-list');
-      bookList.innerHTML = '';
-  
-      books.forEach((book) => {
-        const bookElement = document.createElement('div');
-        bookElement.classList.add('book');
-        bookElement.innerHTML = `
+  static displayBooks() {
+    const books = Store.getBooks();
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+
+    books.forEach((book) => {
+      const bookElement = document.createElement('div');
+      bookElement.classList.add('book');
+      bookElement.innerHTML = `
               <div>
               <span><strong>Title:</strong> ${book.title}</span>
               <span><strong>Author:</strong> ${book.author}</span>
               </div>
               <button class="delete">Remove</button>
             `;
-        bookList.appendChild(bookElement);
-      });
-    }
-  
-    static addBook() {
-      const titleInput = document.getElementById('titleInput');
-      const authorInput = document.getElementById('authorInput');
-      const title = titleInput.value.trim();
-      const author = authorInput.value.trim();
-  
+      bookList.appendChild(bookElement);
+    });
+  }
+
+  static addBook() {
+    const titleInput = document.getElementById('titleInput');
+    const authorInput = document.getElementById('authorInput');
+    const title = titleInput.value.trim();
+    const author = authorInput.value.trim();
+
+    const book = new Book(title, author);
+    Store.addBook(book);
+    UI.displayBooks();
+    titleInput.value = '';
+    authorInput.value = '';
+  }
+
+  static removeBook(target) {
+    if (target.classList.contains('delete')) {
+      const bookElement = target.parentElement;
+      const title = bookElement.querySelector('span:first-child').textContent.split(': ')[1];
+      const author = bookElement.querySelector('span:nth-child(2)').textContent.split(': ')[1];
       const book = new Book(title, author);
-      Store.addBook(book);
+      Store.removeBook(book);
       UI.displayBooks();
-      titleInput.value = '';
-      authorInput.value = '';
-    }
-  
-    static removeBook(target) {
-      if (target.classList.contains('delete')) {
-        const bookElement = target.parentElement;
-        const title = bookElement.querySelector('span:first-child').textContent.split(': ')[1];
-        const author = bookElement.querySelector('span:nth-child(2)').textContent.split(': ')[1];
-        const book = new Book(title, author);
-        Store.removeBook(book);
-        UI.displayBooks();
-      }
     }
   }
+}
 
 // Event listeners
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -63,11 +62,11 @@ bookList.addEventListener('click', (e) => {
 });
 
 setInterval(() => {
-    const date = DateTime.now();
-    document.querySelector('#date').innerHTML = date.toLocaleString(
-      DateTime.DATETIME_MED,
-    );
-  }, 1000);
+  const date = DateTime.now();
+  document.querySelector('#date').innerHTML = date.toLocaleString(
+    DateTime.DATETIME_MED,
+  );
+}, 1000);
 
 const list = document.getElementById('list');
 list.addEventListener('click', (e) => {
