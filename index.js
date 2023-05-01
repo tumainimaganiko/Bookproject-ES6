e60872dd00d8c2fb11f7862918d5cb5d20a1b976
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-01 18:32:55 +0300

    Add Section functions

--- index.js
+++ index.js
@@ -45,3 +45,57 @@ class UI {
       }
     }
   }
+
+// Event listeners
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+const addBookForm = document.getElementById('addBookForm');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  UI.addBook();
+});
+
+const bookList = document.getElementById('book-list');
+bookList.addEventListener('click', (e) => {
+  UI.removeBook(e.target);
+});
+
+
+const list = document.getElementById('list');
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+  const booklist = document.querySelector('#book-list');
+  const addBookForm = document.querySelector('#addBookForm');
+  const contact = document.querySelector('.contact');
+  const heading = document.querySelector('#heading1');
+  booklist.style.display = 'block';
+  addBookForm.style.display = 'none';
+  heading.style.display = 'block';
+  contact.style.display = 'none';
+});
+
+const add = document.getElementById('add');
+add.addEventListener('click', (e) => {
+  e.preventDefault();
+  const addBookForm = document.querySelector('#addBookForm');
+  const heading = document.querySelector('#heading1');
+  const contact = document.querySelector('.contact');
+  addBookForm.style.display = 'flex';
+  const booklist = document.querySelector('#book-list');
+  booklist.style.display = 'none';
+  contact.style.display = 'none';
+  heading.style.display = 'none';
+});
+
+const contact = document.getElementById('contact');
+contact.addEventListener('click', (e) => {
+  e.preventDefault();
+  const addBookForm = document.querySelector('#addBookForm');
+  const heading = document.querySelector('#heading1');
+  const contact = document.querySelector('.contact');
+  addBookForm.style.display = 'none';
+  const booklist = document.querySelector('#book-list');
+  contact.style.display = 'flex';
+  booklist.style.display = 'none';
+  heading.style.display = 'none';
+});
